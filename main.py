b768884c349b8f25b6c0788ae71435c0093cab97
gianl <gianluca.botteri@gmail.com>
2020-01-27 16:45:26 +0100

    test on opening files.

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+
+
 def main():
-    pass
+    openFile("train.0.txt")
+
+
+def openFile(name):
+    #f = open(name, "r")
+    s = []
+    #for str in f:
+     #   f.readline()
+      #  print(len(str))
+       # s.append([str])
+    #print(s)
+
+    with open(name, 'r') as file:
+        for line in file:
+            text = line.sp
+            s.append(text)
+    print(s)
 
 
 if __name__ == '__main__':
