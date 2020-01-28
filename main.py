1c866bdd790111daedf79619f41f05594b324a7e
gianl <gianluca.botteri@gmail.com>
2020-01-28 02:19:56 +0100

    Added TODO and created dualPerceptron method.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     first = openFile("train.3.txt")
+    # TODO crea altre funzioni per creare il dataset e sposta il main
 
 
 def openFile(name):
@@ -11,6 +12,18 @@ def openFile(name):
     f.close()
     print(len(testList))
     return testList
+
+
+def dualPerceptron():
+    # TODO finisci di implementarlo
+    alfa = 0
+    b = 0
+    R = max()
+    errors = 0
+    while True:
+        if errors == 0:
+            break
+    return alfa, b
 
 
 if __name__ == '__main__':
