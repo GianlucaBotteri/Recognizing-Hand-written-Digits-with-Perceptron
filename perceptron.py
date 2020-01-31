3344e0b2b42b5b2e19687d1beba78cee2f736fb0
gianl <gianluca.botteri@gmail.com>
2020-01-31 16:50:01 +0100

    Refactoring code.

--- perceptron.py
+++ perceptron.py
@@ -15,26 +15,21 @@ def computeR(examples):
 def dualFormPerceptron(dataset, R, gamma):
     b = 0
     examples = list(dataset.keys())
-    print("Il valore di R è: ", R)
     alfa = [0] * len(examples)
-    while True:
-        errors = 0
+    errors = -1  # Inizializzo il numero di errori ad un valore fittizio per entrare nel ciclo while
+    while errors != 0:
+        errors = 0  # Azzero il numero di errori prima dell'esecuzione del ciclo for
         for i in range(len(examples)):
             summation = 0
             for j in range(len(examples)):
-                sub = []
                 module = 0
-                for k in range(256):
-                    sub.append((examples[j])[k] - (examples[i])[k])
-                for k in range(256):
-                    module += math.pow(sub[k], 2)
-                summation += alfa[j]*dataset[examples[j]]*math.exp(-gamma*module)
-            if dataset[examples[i]]*(summation+b) <= 0:
+                for k in range(256):  # 256 sono il numero di valori fissati per ogni esempio del dataset
+                    module += math.pow((examples[j])[k] - (examples[i])[k], 2)
+                summation += alfa[j]*dataset[examples[j]]*math.exp(-gamma*module)  # Calcola la sommatoria per ogni j
+            if dataset[examples[i]]*(summation+b) <= 0:  # Controlla se la classificazione è sbagliata
                 alfa[i] += 1
                 b += dataset[examples[i]]*math.pow(R, 2)
-                errors += 1
-        if errors == 0:
-            break
+                errors += 1  # Incremento il numero di errori
     print(alfa)
     print(b)
     return alfa, b
